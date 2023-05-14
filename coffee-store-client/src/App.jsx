64d20d0597ece19bc7ac5f88892fee8ec7ffa539
Iftekher Aziz <azizchy02@gmail.com>
2023-05-14 15:21:55 +0600

    commit

---
+++ coffee-store-client/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-purple-600 text-center">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
